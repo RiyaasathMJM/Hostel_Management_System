f4e41754e94e699d680c47f737fe8b28cf833d59
RiyaasathMJM <riyasathmjm@gmail.com>
2025-06-01 18:35:26 +0530

    Databaseconnection checking

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php
 
 include("header.html");
-
-
 include("db_connect.php");
+
+// Sample student data
+$name = "Usman";
+$department = "com";
+$gender = "M";
+$address = "nintavur_Ampara";
+$contact_no = "938"; 
+$year_of_study = 3;
+$username = "2022e096";
+$password = "dd";
+
+// Hash the password
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+// SQL to insert a student (exclude stu_id since it's AUTO_INCREMENT)
+$sql = "INSERT INTO student (name, department, gender, address, contact_no, year_of_study, username, password)
+        VALUES ('$name', '$department', '$gender', '$address', '$contact_no', $year_of_study, '$username', '$hash')";
+
+// Execute and check
+if (mysqli_query($conn, $sql)) {
+    echo "Inserted successfully";
+} else {
+    echo "Insert failed: " . mysqli_error($conn);
+}
+
 ?>
+
