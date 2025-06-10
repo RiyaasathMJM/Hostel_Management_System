3b3aa3ea9e9d792474866f403ccf1b241dfdb9a5
RiyaasathMJM <riyasathmjm@gmail.com>
2025-06-10 20:40:28 +0530

    Student Dashboard created

---
+++ student_dashboard.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] !== 'student') {
+    header("Location: login.html");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Dashboard</title>
+</head>
+<body>
+    <h2>Welcome Student, <?php echo $_SESSION['username']; ?>!</h2>
+
+    <ul>
+        <li><a href="#">View Room Details</a></li>
+        <li><a href="#">View Payment Info</a></li>
+        <li><a href="#">Submit Request</a></li>
+        <li><a href="#">View Hostel Rules</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</body>
+</html>
