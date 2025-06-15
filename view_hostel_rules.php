48a02fa16884e22da5acd9a185367f74b0ffcddd
RiyaasathMJM <riyasathmjm@gmail.com>
2025-06-15 18:51:43 +0530

    Created

---
+++ view_hostel_rules.php
@@ -0,0 +1,8 @@
+<h2>Hostel Rules</h2>
+<ul>
+    <li>Maintain silence after 10 PM</li>
+    <li>No cooking in rooms</li>
+    <li>Visitors are not allowed in rooms</li>
+    <li>Keep your room clean</li>
+</ul>
+<a href="student_dashboard.php">Back</a>
