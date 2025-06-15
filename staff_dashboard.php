9a7da78c22cd09200b7985741fd059debaf3927d
RiyaasathMJM <riyasathmjm@gmail.com>
2025-06-15 18:49:18 +0530

    Links updated

--- staff_dashboard.php
+++ staff_dashboard.php
@@ -15,9 +15,9 @@ if (!isset($_SESSION['user_type']) || $_SESSION['user_type'] !== 'staff') {
     <h2>Welcome Staff, <?php echo $_SESSION['username']; ?>!</h2>
 
     <ul>
-        <li><a href="#">View Room Details</a></li>
-        <li><a href="#">Submit Maintenance Request</a></li>
-        <li><a href="#">View Hostel Rules</a></li>
+        <li><a href="view_room_details.php">View Room Details</a></li>
+        <li><a href="submit_request.php">Submit Request</a></li>
+        <li><a href="maintenance_request.php">Submit Maintenance Request</a></li>
         <li><a href="logout.php">Logout</a></li>
     </ul>
 </body>
